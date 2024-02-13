d6ac01e2d3887c99c7a00f87ffd6869914c63306
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-02-13 14:35:20 +0200

    added task_eight

--- Day2/lab_02.py
+++ Day2/lab_02.py
@@ -117,4 +117,32 @@ def task_seven(string):
             word=''
     keys = list(dict.keys())
     return dict[max(keys)]
-print(task_seven('abdulrahman'))
+# print(task_seven('abdulrahman'))
+
+
+"""
+Write a program which repeatedly reads numbers until the user
+enters “done”.
+    ○ Once “done” is entered, print out the total, count, and
+    average of the numbers.
+    ○If the user enters anything other than a number, detect their
+    mistake, print an error message and skip to the next number.
+"""
+
+def task_eight():
+    count = 0
+    total = 0
+    while True:
+        number = input("Enter a number ")
+        if number == "done":
+            print(f"total= {total} count={count} avg={total/count}")
+            break
+        elif number.isdigit():
+            number =  int(number)
+            count+=1
+            total+=number
+        else:
+            print("Enter vaild number")
+task_eight()
+
+
